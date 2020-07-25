77dcdd1b0b9b99861c74e07e96008affbbbc96d9
Michael <mikrlynch@gmail.com>
2020-07-25 16:04:55 -0400

    Added docstring and formatting

--- banditfuzz/fuzzers/bv/variable.py
+++ banditfuzz/fuzzers/bv/variable.py
@@ -2,11 +2,14 @@ from ..variable import Variable
 
 
 class BitVec_Variable(Variable):
+    """ Class to implement the bitvector variable """
+
     def __init__(self, name, num):  # num is length
         self.sort = 'bv'
         self.name, self.num = name, num
 
     def declare(self):
+        """ Returns a statement of declaration """
         return f'(declare-const {self.name} (_ BitVec {self.num}))'
 
     def __str__(self):
