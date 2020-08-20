fb6ecd51b8ad69fa6e5c6966b405b0708275c3a4
Michael <mikrlynch@gmail.com>
2020-08-20 14:34:03 -0400

    Added BV variable

---
+++ banditfuzz/fuzzers/bv/variable.py
@@ -0,0 +1,13 @@
+from ..variable import Variable
+
+
+class BV_Variable(Variable):
+    def __init__(self, name, width):
+        self.sort = 'BitVec'
+        self.name, self.width = name, width
+
+    def declare(self):
+        return f'(declare-const {self.name} (_ FloatingPoint {self.width}))'
+
+    def __str__(self):
+        return self.name
