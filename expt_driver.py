1d97f9f5aa19e10897234108d73035377c1cbdcb
Michael <mikrlynch@gmail.com>
2020-08-23 12:45:13 -0400

    Set up presentation experiment.

--- expt_driver.py
+++ expt_driver.py
@@ -5,17 +5,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def remove_last_line_from_string(s):
-    return s[:s.rfind('\n')]
+def remove_last_line_from_string(string):
+    """ Removes the last \n ended portion of the string"""
+    return string[:string.rfind('\n')]
 
 
-def trim_input(s):
-    return remove_last_line_from_string(remove_last_line_from_string(s))
+def trim_input(string):
+    """ Removes last two lines """
+    return remove_last_line_from_string(remove_last_line_from_string(string))
 
 
 def fuzz_solvers(width, depth=5, num_vars=4, num_ast=5):
+    """ Creates an input and evaluates it on all solvers """
     print("Width: {0}\tDepth {1}\tNum vars: {2}\tNum ASTs: {3}".format(
         width, depth, num_vars, num_ast))
+    # Build the banditfuzz command string
     cmd_bf = ['python', './bin/banditfuzz',
               '-d', str(depth),
               '-v', str(num_vars),
@@ -26,18 +30,22 @@ def fuzz_solvers(width, depth=5, num_vars=4, num_ast=5):
               '../runs/cvc4',
               '../runs/z3',
               '../runs/boolector']  # target solvers
+    # Get the solver input
     banditfuzz_out = subprocess.run(cmd_bf,
                                     capture_output=True, check=True).stdout
     solver_input = trim_input(banditfuzz_out.decode()).encode()
+    # Creates unique filename for input
     uuid_str = str(uuid.uuid4())
     filename = "./inputs/{0}.smt2".format(uuid_str)
-    with open(filename, 'w+b') as fp:
-        fp.write(solver_input)
-
+    print("Input file: {0}".format(filename))
+    # Write input to file
+    with open(filename, 'w+b') as inp_file:
+        inp_file.write(solver_input)
+    # Creates command strings for solvers
     cmd_z3 = ['../runs/z3', filename]
     cmd_cvc4 = ['../runs/cvc4', '--lang', 'smt', filename]
     cmd_boolector = ['../runs/boolector',  filename]
-    # breakpoint()
+
     try:
         z3_s = time.time()
         z3_output = subprocess.run(cmd_z3,
@@ -47,9 +55,10 @@ def fuzz_solvers(width, depth=5, num_vars=4, num_ast=5):
         z3_e = time.time()
         z3_runtime = z3_e - z3_s
     except subprocess.TimeoutExpired:
-        z3_output = "timed out!"
+        z3_output = "timeout"
         z3_runtime = INF
         # breakpoint()
+    print("Z3: ", z3_output)
     try:
         cvc4_s = time.time()
         cvc4_output = subprocess.run(cmd_cvc4,
@@ -59,9 +68,10 @@ def fuzz_solvers(width, depth=5, num_vars=4, num_ast=5):
         cvc4e = time.time()
         cvc4_runtime = cvc4e - cvc4_s
     except subprocess.TimeoutExpired:
-        cvc4_output = "timed out!"
+        cvc4_output = "timeout"
         cvc4_runtime = INF
 
+    print("CVC4: ", cvc4_output)
     try:
         # breakpoint()
         boolector_s = time.time()
@@ -75,31 +85,37 @@ def fuzz_solvers(width, depth=5, num_vars=4, num_ast=5):
     except subprocess.TimeoutExpired:
         blctr_output = "timeout"
         boolector_runtime = INF
-
+    print("Boolector: ", blctr_output)
     runtime_data = [width, depth, num_vars, num_ast,
                     z3_runtime, cvc4_runtime, boolector_runtime]
 
-    print("Input file: {0}".format(filename))
-    print("Z3: ", z3_output)
-    print("CVC4: ", cvc4_output)
-    print("Boolector: ", blctr_output)
-
-    if not (z3_output == cvc4_output == blctr_output):
-
+    if not z3_output == cvc4_output == blctr_output:
+        # If discrepancies in outputs
+        # Log it
         log_str = "/*-----------------------------------------------------*/"
         log_str += "\n Z3 {0}\t CVC4 {1}\t Boolector {2}\n".format(
             z3_output, cvc4_output, blctr_output)
-        log_str += "INPUT: \n {0}\n".format(solver_input.decode())
+        log_str += "INPUT: \n {0}\n".format(filename)
         log_str += "/*----------------------------------------------------*/"
         log_str += "\n"
         with open(LOGFILE, 'a') as lgfile:
             lgfile.write(log_str)
 
+        # If we have SAT and unSAT: big problem!
+        solver_outputs = [z3_output, cvc4_output, blctr_output]
+        if 'sat' in solver_outputs and 'unsat' in solver_outputs:
+            with open(INCONSISTENCIES, 'a') as inconfile:
+                inconfile.write(log_str)
+    else:
+        print("Finished this round")
+
     return runtime_data
 
 
 def cactus_plots(input_data, variable):
+    """ Creates cactus plot of experiments """
     runtime_data = np.array(input_data)
+
     if variable == 'width':
         var_xlabel = "Bitvector Width"
         var_idx = 0
@@ -136,39 +152,20 @@ def cactus_plots(input_data, variable):
 
 
 LOGFILE = "./inputs/runlogs.txt"
+INCONSISTENCIES = "./inputs/incons.txt"
 INF = 1000000
-TIMEOUT = 360
-MIN_WIDTH, MAX_WIDTH = (1, 64)
-MIN_DEPTH, MAX_DEPTH = (1, 20)
+TIMEOUT = 20
+MIN_WIDTH, MAX_WIDTH = (1, 8)
+MIN_DEPTH, MAX_DEPTH = (1, 10)
 MIN_NUMVARS, MAX_NUMVARS = (1, 10)
-MIN_NUMASTS, MAX_NUMASTS = (1, 20)
+MIN_NUMASTS, MAX_NUMASTS = (1, 10)
 
-width = range(MIN_WIDTH, MAX_WIDTH+1)
-depth = range(MIN_DEPTH, MAX_DEPTH+1)
+widths = range(MIN_WIDTH, MAX_WIDTH+1)
+depths = range(MIN_DEPTH, MAX_DEPTH+1)
 numvars = range(MIN_NUMVARS, MAX_NUMVARS+1)
 numasts = range(MIN_NUMASTS, MAX_NUMASTS+1)
 
-# width_data = [fuzz_solvers(width=w, depth=6, num_vars=4, num_ast=5)
-#               for w in width]
-# depth_data = [fuzz_solvers(width=w)
-#               for w in width]
-depth_data = [fuzz_solvers(width=16, depth=d)
-              for d in depth]
-numvar_data = [fuzz_solvers(width=16, num_vars=v)
-               for v in numvars]
-numast_data = [fuzz_solvers(width=16,  num_ast=a)
-               for a in numasts]
+width_data = [fuzz_solvers(width=w, depth=8,  num_ast=15, num_vars=6)
+              for w in widths]
 
-# cactus_plots(width_data, 'width')
-cactus_plots(depth_data, 'depth')
-cactus_plots(numvar_data, 'numvars')
-cactus_plots(numast_data, 'numasts')
-#width_data = [fuzz_solvers(width=w, depth=6, num_vars=4, num_ast=5) for w in width]
-#width_data = [fuzz_solvers(width=w, depth=6, num_vars=4, num_ast=5) for w in width]
-
-
-# breakpoint()
-#width_data = np.array(width_data)
-
-#numvar_data = np.array(numvar_data)
-#numast_data = np.array(numast_data)
+cactus_plots(width_data, 'width')
